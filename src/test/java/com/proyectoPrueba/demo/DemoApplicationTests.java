1f374462ecc5919341b7ee1126d3da6f24ee563b
Yesid Alberto Orrego Restrepo - Ceiba Software <yesid.orrego@ceiba.com.co>
2020-12-16 09:32:49 -0500

    First release

---
+++ src/test/java/com/proyectoPrueba/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoPrueba.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
